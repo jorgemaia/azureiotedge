867684d6e7b9a72ff6567a3f8b5753e4dc6511d2
Sebastian Bader <sebader@microsoft.com>
2019-02-01 13:06:57 +0100

    Revert "Introduced new helper class library to move common code to"
    This reverts commit 48caa0ccd6ff8103f89b222c44ce7091be666210.
    
    Reverted helper class as incompatible docker build process at the moment

--- SampleEdgeSolution/modules/DataGenerator/Program.cs
+++ SampleEdgeSolution/modules/DataGenerator/Program.cs
@@ -1,16 +1,20 @@
 namespace DataGenerator
 {
     using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Runtime.InteropServices;
     using System.Runtime.Loader;
+    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
     using Microsoft.Azure.Devices.Shared;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using Serilog;
-    using helpers;
 
 
     /// <summary>
@@ -32,12 +36,12 @@ namespace DataGenerator
 
         static async Task<int> MainAsync()
         {
-            EdgeModuleHelpers.InitLogging();
+            InitLogging();
             Log.Information($"Module {Environment.GetEnvironmentVariable("IOTEDGE_MODULEID")} starting up...");
-            var moduleClient = await EdgeModuleHelpers.Init();
+            var moduleClient = await Init();
 
             // Get initial device twin
-            Twin twin = await moduleClient.GetTwinAsync();
+            var twin = await moduleClient.GetTwinAsync();
             // Process desired properties
             await ProcessDesiredPropertiesUpdate(twin.Properties.Desired, moduleClient);
 
@@ -64,6 +68,104 @@ namespace DataGenerator
             var tcs = new TaskCompletionSource<bool>();
             cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
             return tcs.Task;
+        }
+
+        /// <summary>
+        /// Initializes the ModuleClient
+        /// </summary>
+        static async Task<ModuleClient> Init()
+        {
+            var transportType = TransportType.Mqtt_Tcp_Only;
+            string transportProtocol = Environment.GetEnvironmentVariable("TransportProtocol");
+
+            // The way the module connects to the EdgeHub can be controlled via the env variable. Either MQTT or AMQP
+            if (!string.IsNullOrEmpty(transportProtocol))
+            {
+                switch (transportProtocol.ToUpper())
+                {
+                    case "AMQP":
+                        transportType = TransportType.Amqp_Tcp_Only;
+                        break;
+                    case "MQTT":
+                        transportType = TransportType.Mqtt_Tcp_Only;
+                        break;
+                    default:
+                        // Anything else: use default of MQTT
+                        Log.Warning($"Ignoring unknown TransportProtocol={transportProtocol}. Using default={transportType}");
+                        break;
+                }
+            }
+
+            // Open a connection to the Edge runtime
+            ModuleClient moduleClient = await ModuleClient.CreateFromEnvironmentAsync(transportType);
+            await moduleClient.OpenAsync();
+
+            moduleClient.SetConnectionStatusChangesHandler(ConnectionStatusHandler);
+
+            Log.Information($"Edge Hub module client initialized using {transportType}");
+
+            return moduleClient;
+        }
+
+        /// <summary>
+        /// Callback for whenever the connection status changes
+        /// Mostly we just log the new status and the reason. 
+        /// But for some disconnects we need to handle them here differently for our module to recover
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="reason"></param>
+        private static void ConnectionStatusHandler(ConnectionStatus status, ConnectionStatusChangeReason reason)
+        {
+            Log.Information($"Module connection changed. New status={status.ToString()} Reason={reason.ToString()}");
+
+            // Sometimes the connection can not be recovered if it is in either of those states.
+            // To solve this, we exit the module. The Edge Agent will then restart it (retrying with backoff)
+            if (reason == ConnectionStatusChangeReason.Retry_Expired)
+            {
+                Log.Error($"Connection can not be re-established. Exiting module");
+                Environment.Exit(1);
+            }
+        }
+
+        /// <summary>
+        /// Initialize logging using Serilog
+        /// LogLevel can be controlled via RuntimeLogLevel env var
+        /// </summary>
+        private static void InitLogging()
+        {
+            LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
+
+            var logLevel = Environment.GetEnvironmentVariable("RuntimeLogLevel");
+            logLevel = !string.IsNullOrEmpty(logLevel) ? logLevel.ToLower() : "info";
+
+            // set the log level
+            switch (logLevel)
+            {
+                case "fatal":
+                    loggerConfiguration.MinimumLevel.Fatal();
+                    break;
+                case "error":
+                    loggerConfiguration.MinimumLevel.Error();
+                    break;
+                case "warn":
+                    loggerConfiguration.MinimumLevel.Warning();
+                    break;
+                case "info":
+                    loggerConfiguration.MinimumLevel.Information();
+                    break;
+                case "debug":
+                    loggerConfiguration.MinimumLevel.Debug();
+                    break;
+                case "verbose":
+                    loggerConfiguration.MinimumLevel.Verbose();
+                    break;
+            }
+
+            // set logging sinks
+            loggerConfiguration.WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] - {Message}{NewLine}{Exception}");
+            loggerConfiguration.Enrich.FromLogContext();
+            Log.Logger = loggerConfiguration.CreateLogger();
+            Log.Information($"Initializied logger with log level {logLevel}");
         }
 
         /// <summary>
